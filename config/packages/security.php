d25059c19fc9541df1622432e17cf6f4473e5dee
Muneer shafi <zmzahidmuneer@gmail.com>
2023-05-07 00:32:42 +0530

    fix:changes in security file

---
+++ config/packages/security.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Config\SecurityConfig;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+
+return static function (SecurityConfig $security) {
+    $security->passwordHasher(PasswordAuthenticatedUserInterface::class)
+        ->algorithm('auto');
+
+    $security
+        ->provider('app_users')
+        ->entity()
+        ->class(\App\Entity\User::class)
+        ->property('username');
+
+
+    $mainFirewall = $security
+        ->firewall('main')
+        ->lazy(true)
+        ->provider('app_users')
+        //        ->entryPoint(AuthenticationEntryPoint::class)
+        //        ->customAuthenticators([UserAuthenticator::class])
+        ->lazy(true);
+        $mainFirewall->entryPoint('form_login');
+    $mainFirewall->formLogin()
+        ->loginPath('security_login')
+        ->checkPath('security_login')
+        ->enableCsrf(true)
+        ->defaultTargetPath('blog_index');
+
+    $mainFirewall->rememberMe()
+        ->secret('%kernel.secret%')
+        ->lifetime(604800);
+
+    $mainFirewall->logout()
+        ->path('security_logout')
+        ->target('homepage')
+        ->invalidateSession(true);
+
+    $security->accessControl()
+        ->path('^/(%app_locales%)/admin')
+        ->roles('ROLE_ADMIN');
+
+    $security->roleHierarchy('ROLE_ADMIN', ['ROLE_USER', 'ROLE_STUDENT']);
+    $security->roleHierarchy('ROLE_SUPER_ADMIN', ['ROLE_ADMIN', 'ROLE_ALLOWED_TO_SWITCH']);
+
+    $devFireWall = $security->firewall('development')
+        ->pattern('^/(_(profiler|wdt)|css|images|js)/')
+        ->security(false);
+
+    $security
+        ->passwordHasher(PasswordAuthenticatedUserInterface::class)->algorithm('auto')
+        ->cost(4)
+        ->timeCost(3)
+        ->memoryCost(10);
+    $security->roleHierarchy('ROLE_ADMIN', ['ROLE_USER']);
+    $security->roleHierarchy('ROLE_SUPER_ADMIN', ['ROLE_ADMIN', 'ROLE_ALLOWED_TO_SWITCH']);
+};
