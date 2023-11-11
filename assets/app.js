46ac3f5c31cc268e4cc04aa844f453dc8a87051d
Muneer shafi <zmzahidmuneer@gmail.com>
2023-11-11 22:09:08 +0530

    ui : adding new styles

--- assets/app.js
+++ assets/app.js
@@ -3,9 +3,25 @@
 import './bootstrap';
 // assets/app.js
 import { registerReactControllerComponents } from '@symfony/ux-react';
-
+import "@material/web/icon/icon.js";
+import "@material/web/button/elevated-button.js";
 import "@material/web/button/filled-button.js";
 import "@material/web/button/outlined-button.js";
-import "@material/web/checkbox/checkbox.js";
+import "@material/web/button/text-button.js";
+import "@material/web/button/filled-tonal-button.js";
+
+
+
+import "@material/web/menu/menu-item.js";
+import "@material/web/menu/sub-menu.js";
+import "@material/web/menu/menu.js";
+import "@material/web/divider/divider.js";
+
+
+
+import "@material/web/iconbutton/icon-button.js";
+import "@material/web/tabs/tabs.js";
+import "@material/web/tabs/primary-tab.js";
+import "@material/web/tabs/secondary-tab.js";
 
 registerReactControllerComponents(require.context('./react/controllers', true, /\.(j|t)sx?$/));
